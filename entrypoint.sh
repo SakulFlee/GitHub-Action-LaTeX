9d605cd6f3ae2aac730a47ab265b5b1c7db0849d
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2021-01-17 17:06:50 +0100

    Added entrypoint outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ POST_TASK="${7}"
 
 # --- Print Options ---
 echo ""
-echo "Displaying set options:"
+echo ">>> Options:"
 echo "LATEX_MAIN_FILE: $LATEX_MAIN_FILE"
 echo "WORKING_DIRECTORY: $WORKING_DIRECTORY"
 echo "LATEX_COMPILER: $LATEX_COMPILER"
@@ -23,20 +23,27 @@ echo "POST_TASK: $POST_TASK"
 
 # --- Check Options ---
 if [ -z "$LATEX_MAIN_FILE" ]; then
+    echo ""
     echo "No main file option was set (or is empty)!"
     exit 1
 fi
 
 if [ -z "$LATEX_COMPILER" ]; then
+    echo ""
     echo "The set LaTeX compiler is empty!"
     exit 1
 fi
 
 # --- Install packages ---
 if [ ! -z "$SYSTEM_PACKAGES" ]; then
+    echo ""
+    echo ">>> Installing system packages ..."
+
     pacman -Sy
     pacman -S --noconfirm "$SYSTEM_PACKAGES"
+
     if [ $? -ne 0 ]; then
+        echo ""
         echo "Failed installing system packages!"
         echo "Check the package names and if they exist in Arch (not AUR!)."
         exit 1
@@ -45,17 +52,29 @@ fi
 
 # --- Pre Task ---
 if [ ! -z "$PRE_TASK" ]; then 
+    echo ""
+    echo ">>> Running pre task"
+
     eval "$PRE_TASK"
 fi
 
 # --- Compile Document ---
 if [ ! -z "$WORKING_DIRECTORY" ]; then
+    echo ""
+    echo ">>> Changing directory"
+
     cd "$WORKING_DIRECTORY"
 fi
+
+echo ""
+echo ">>> Compiling document"
 
 eval "$LATEX_COMPILER $LATEX_COMPILER_ARGUMENTS $LATEX_MAIN_FILE"
 
 # --- Post Task ---
 if [ ! -z "$POST_TASK" ]; then 
+    echo ""
+    echo ">>> Running post task"
+
     eval "$POST_TASK"
 fi
